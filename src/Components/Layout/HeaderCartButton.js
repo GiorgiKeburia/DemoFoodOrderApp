370002663b1ab839691ce4e78b2cb9767b2262df
giorgikeburia <giorgikeburia1@gmail.com>
2021-11-03 22:57:19 +0400

    Create cart logic

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,13 +1,22 @@
+import { useContext } from 'react';
+
+import CartContext from '../../Store/Cart-context';
 import classes from './Header.cart.button.module.css';
 import CartIcon from '../Cart/CartIcon';
 
 const HeaderCartButton = (props) => {
+	const cartCtx = useContext(CartContext);
+
+	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+		return curNumber + item.amount;
+	}, 0);
+
 	return (
 		<button onClick={props.onClick} className={classes.button}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
-			<span className={classes.badge}>8</span>
+			<span className={classes.badge}>{numberOfCartItems}</span>
 		</button>
 	);
 };
